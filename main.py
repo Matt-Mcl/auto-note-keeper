4a00ee5635b113c90bf80fcbe51fc2f80d0769c2
Matt-Mcl <mattmcl2000@gmail.com>
2022-02-28 12:48:28 +0000

    Adds comments and note logging

--- main.py
+++ main.py
@@ -12,22 +12,25 @@ def main():
     email = os.getenv('EMAIL')
     secret = os.getenv('SECRET')
     keep.login(email, secret)
-
+    # Find existing note
     note = keep.find(query='Raspberry Pi Auto Note')
     try:
+        # Delete existing note
         next(note).delete()
     except StopIteration:
         # Note doesn't exist
         pass
-
+    # Get IP address
     ip = requests.get('https://ifconfig.me/')
+    # Get temperature
     temp_command = ["cat", "/sys/class/thermal/thermal_zone0/temp"]
     process = subprocess.Popen(temp_command, stdout=subprocess.PIPE)
     output, error = process.communicate()
-
+    # Format temperature
     output = int(output.decode('ascii')[:-1])/1000
-
-    note = keep.createNote('Raspberry Pi Auto Note', f"IP: {ip.text}\nTemperature: {output}°C")#
+    # Create note
+    note = keep.createNote('Raspberry Pi Auto Note', f"IP: {ip.text}\nTemperature: {output}°C")
+    print(note)
     note.color = gkeepapi.node.ColorValue.Yellow
 
     keep.sync()
