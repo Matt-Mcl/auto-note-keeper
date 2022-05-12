7c42c6df7e9a20c8af34a63c71848be74957ba2b
Matt-Mcl <mattmcl2000@gmail.com>
2022-05-12 11:44:30 +0000

    Optimises imports

--- main.py
+++ main.py
@@ -1,31 +1,31 @@
-import os
-import pytz
-import requests
-import subprocess
+from os import getenv
+from pytz import timezone
+from requests import put, get
+from subprocess import check_output
 from dotenv import load_dotenv
 from datetime import datetime
 
 load_dotenv()
 
-key = os.getenv('KEY')
-token = os.getenv('TOKEN')
-cards = os.getenv('CARDS').split(',')
+key = getenv('KEY')
+token = getenv('TOKEN')
+cards = getenv('CARDS').split(',')
 
 # Get time
-now = datetime.now(pytz.timezone("Europe/London")).strftime("%d/%m/%Y, %H:%M")
+now = datetime.now(timezone("Europe/London")).strftime("%d/%m/%Y, %H:%M")
 
 # Get IP address
-ip = requests.get('https://ifconfig.me/')
+ip = get('https://ifconfig.me/')
 
 # Get and format temperature
 temp_command = ["cat", "/sys/class/thermal/thermal_zone0/temp"]
-output = subprocess.check_output(temp_command)
+output = check_output(temp_command)
 temperature = int(output.decode('ascii')[:-1])/1000
 
 notes = [f"Time: {now}", f"IP: {ip.text}", f"Temperature: {temperature}°C"]
 
 for note, card_id in zip(notes, cards):
-    requests.put(
+    put(
         url = f"https://api.trello.com/1/cards/{card_id}", 
         params = {
             "key": key,
